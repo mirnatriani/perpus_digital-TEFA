9c0879febb41c41e881e5b9c543971f11256e692
Mirna Triani <141211528+mirnatriani@users.noreply.github.com>
2024-05-14 10:56:16 +0700

    Update Header.vue

--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="row p-3 bg-primary text-white">
     <div class="col-lg-1">
-      <img src="~/assets/img/LOGO-SMK4.png" alt="logo SMKN 4" />
+      <img src="~/assets/img/LOGO-SMK4.webp" alt="logo SMKN 4" />
     </div>
 
     <div class="col-lg-3 ms-3">
